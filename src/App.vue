bf6dadd80b162c5b78d52ee5db36bd2d3504ac44
Obada Khalili <obada.akhalili@gmail.com>
2023-04-27 10:11:33 +0200

    implement auth

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+
+  import { Amplify } from 'aws-amplify';
+  import awsconfig from './aws-exports.js';
+
+  Amplify.configure(awsconfig);
+</script>
+
 <template>
-  <h1>CSV Repo</h1>
+  <Authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </Authenticator>
 </template>
